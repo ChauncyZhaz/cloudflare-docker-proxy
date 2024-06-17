f9485abf3b72e490ffaaaf46766df1f10cedb782
ChauncyZhaz <49758521+ChauncyZhaz@users.noreply.github.com>
2024-06-17 16:31:10 +0800

    Update index.js
    1

--- src/index.js
+++ src/index.js
@@ -7,13 +7,13 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.zhacheng.top": dockerHub,
+  "quay.zhacheng.top": "https://quay.io",
+  "gcr.zhacheng.top": "https://gcr.io",
+  "k8s-gcr.zhacheng.top": "https://k8s.gcr.io",
+  "k8s.zhacheng.top": "https://registry.k8s.io",
+  "ghcr.zhacheng.top": "https://ghcr.io",
+  "cloudsmith.zhacheng.top": "https://docker.cloudsmith.io",
 
   // staging
   "docker-staging.libcuda.so": dockerHub,
